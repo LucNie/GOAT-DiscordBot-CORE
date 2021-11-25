ac751d12048ed1906313f0fa8c0b952f5077fcef
LucNie <lucas.niemerich@epitech.eu>
2021-11-25 17:37:00 +0100

    adding storage procedure

--- commands/userAdd.js
+++ commands/userAdd.js
@@ -1,16 +1,36 @@
+const fs = require('fs');
 module.exports = {
     name: 'adduser',
     description: 'Ajoute un utilisateur',
     level: 3, //modo
   
-    execute(message) {
-         const User = function(name) {
+    execute(message, args) {
+
+      if(args[0] < 0 || args[0] > 5){
+        message.channel.send("Le niveau d'acréditation ne doit pas etre supérieure a 5 !");
+        return;
+      }
+
+      const path = 'users/user_'+message.author.id+".json";
+
+        try {
+          if (fs.existsSync(path)) {
+            return message.channel.send("l'utilisateur "+ message.author.username + " existe deja!")//file exists
+          }
+        } catch(err) {
+          console.error(err)
+        }
+
+         const User = function(name, level) {
 
             this.name= name;
             this.level= 0;
             this.inventaire= [];
             this.class = "employer";
             this.race= "Unknown";
+            this.bio= "";
+            this.statue="hello i'm new";
+            this.status="free";//work//busy...
     
             this.baseLife= 10;     //base 
             this.baseMana= 0;
@@ -26,8 +46,19 @@ module.exports = {
             this.chance= 1;
       }
 
-      eval("var user_"+message.author.id+" = new User('"+message.author.username+"');")
+
+
+
+
+      eval("var user_"+message.author.id+" = new User('"+message.author.username+"', "+args[0]+");")
       message.channel.send(eval("user_"+message.author.id+".name"));
+
+      fs.writeFile('users/user_'+message.author.id+'.json', JSON.stringify(eval("user_"+message.author.id)), (err) => {  
+        // Catch this!
+        if (err) throw err;
+    
+        console.log('Users saved!');
+    });
 
     }
   };
